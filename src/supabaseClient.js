37c60e55f68d631d9b4db99d90f1d9487a5bce91
Pedro Henrique <pedrohenripp6@gmail.com>
2026-06-14 23:12:33 -0300

    configura variáveis de ambiente

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 
 import { createClient } from "@supabase/supabase-js"
 
-  const URL = 'https://rjfdnxdtbckigdpdgkzx.supabase.co'
-  const CHAVE = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqZmRueGR0YmNraWdkcGRna3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NzA3NTUsImV4cCI6MjA5NzA0Njc1NX0.x3UZQOMzFp0lFRh_Rofn-freIflVpBfGSKxzYe9bwN4'
+const URL = import.meta.env.VITE_SUPABASE_URL
+const CHAVE = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 const supabase = createClient(URL, CHAVE)
 
